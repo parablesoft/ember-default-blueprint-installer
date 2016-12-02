2eb1d7131c95c3e915136b5bfcf0c6b80422abac
Vic Amuso <vic@parablesoft.com>
2016-12-02 16:56:12 -0500

    Forgot the o parameter

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-curl route.js https://raw.githubusercontent.com/parablesoft/ember-default-blueprint-installer/master/blueprints/route.js
+curl -o route.js https://raw.githubusercontent.com/parablesoft/ember-default-blueprint-installer/master/blueprints/route.js
